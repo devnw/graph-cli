39ca3d0dd5ddfdfb46030de545f6646cf8549809
Benji Vesterby <benji@devnw.com>
2019-10-11 09:28:40 -0700

    Working on building out the main file for the graph cli

--- main.go
+++ main.go
@@ -1,12 +1,68 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+const fileExtension = ".graph"
+
 func main() {
-	// TODO: Parse arguments
+	var err error
 
-	// TODO: Load File
+	filePath := flag.String("file", "", "The path to the file to load the graph from")
+	flag.Parse()
 
-	// TODO: Parse file
+	var valid bool
+	if valid, err = validFile(*filePath); valid && err == nil {
 
-	// TODO: Build graph
+		var extension = filepath.Ext(*filePath)
+		if fileExtension == extension {
 
+			log.Printf("[%s] is a valid file", *filePath)
+			// TODO: Parse arguments
+
+			// TODO: Load File
+
+			// TODO: Parse file
+
+			// TODO: Build graph
+		} else {
+			log.Fatalf("[%s] is an invalid file extension, expected [%s]", extension, fileExtension)
+		}
+	} else {
+
+		var errorText string
+		if err != nil {
+			errorText = err.Error()
+		}
+
+		log.Fatalf("error occurred while validating file: [%s]", errorText)
+	}
+}
+
+func validFile(path string) (valid bool, err error) {
+	var file os.FileInfo
+
+	if len(path) > 0 {
+		if file, err = os.Stat(path); err == nil {
+			if file != nil {
+				if !file.IsDir() {
+					valid = true
+				} else {
+					err = errors.Errorf("path [%s] is a directory", path)
+				}
+			} else {
+				err = errors.New("unable to load file stats, file returned nil")
+			}
+		}
+	} else {
+		err = errors.New("file path cannot be empty")
+	}
+
+	return valid, err
 }
