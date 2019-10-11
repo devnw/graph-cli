7637224507ef154dccdb587040b3f02fe905e820
Benji Vesterby <benji@devnw.com>
2019-10-11 12:16:19 -0700

    Adjusting some code in the main file and fixed the link in the readme for the build

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # graph-cli
 Graph CLI
 
-[![Build Status](https://api.travis-ci.org/benjivesterby/graph-cli.svg?branch=master)](https://api.travis-ci.org/benjivesterby/graph-cli.svg?branch=master)
+[![Build Status](https://api.travis-ci.org/benjivesterby/graph-cli.svg?branch=master)](https://travis-ci.org/benjivesterby/graph-cli)
 [![Go Report Card](https://goreportcard.com/badge/github.com/benjivesterby/graph-cli)](https://goreportcard.com/report/github.com/benjivesterby/graph-cli)
 [![GoDoc](https://godoc.org/github.com/benjivesterby/graph-cli?status.svg)](https://godoc.org/github.com/benjivesterby/graph-cli)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -27,104 +28,127 @@ func main() {
 	if len(*filePath) > 0 {
 		path := *filePath
 
-		var valid bool
-		if valid, err = validFile(path); valid && err == nil {
+		var file *os.File
+		var reader *bufio.Reader
+		if file, reader, err = loadFile(path); err == nil {
+			defer file.Close()
 
-			var extension = filepath.Ext(path)
-			if fileExtension == extension {
+			var line string
+			if line, err = reader.ReadString(linedelim); err == nil {
 
-				// Load File
-				var file *os.File
-				if file, err = os.Open(path); err == nil {
-					defer file.Close()
+				var graphy *graph.Graphy
+				if graphy, err = buildGraph(line); err == nil {
 
-					reader := bufio.NewReader(file)
+					index := 1
+					for err == nil {
+						if line, err = reader.ReadString(linedelim); err == nil {
 
-					var line string
-					if line, err = reader.ReadString(linedelim); err == nil {
-						line = clean(line)
-
-						// Parse file
-						if len(line) > 0 {
-							// Parse out the header of the file
-							values := strings.Split(line, " ")
-
-							// The header should only be two fields
-							if len(values) == 2 {
-								wedgy := false
-								dedgy := false
-
-								if values[0] == directed {
-									dedgy = true
-								}
-
-								if values[1] == weighted {
-									wedgy = true
-								}
-
-								graphy := &graph.Graphy{
-									Directional: dedgy,
-									Weighted:    wedgy,
-								}
-
-								index := 1
-								for err == nil {
-									if line, err = reader.ReadString(linedelim); err == nil {
-
-										// Build the graph using the line from the file
-										err = buildLine(graphy, line)
-									}
-
-									index++
-								}
-
-								if err != nil {
-									log.Fatalf("error at line [%v]: [%s]", index, err.Error())
-								}
-							} else {
-								log.Fatalf("file [%s] is empty", path)
-							}
-						} else {
-							log.Fatalf("file [%s] is empty", path)
+							// Build the graph using the line from the file
+							err = buildNode(graphy, line)
 						}
-					} else {
-						log.Fatalf("error while reading line from file [%s]", err.Error())
+
+						index++
 					}
+
+					if err != nil && err != io.EOF {
+						log.Fatalf("error at line [%v]: [%s]", index, err.Error())
+					}
+
 				} else {
-					log.Fatalf("error while reading from file [%s] : [%s]", path, err.Error())
+					log.Fatalf("error in file [%s]: [%s]", path, err.Error())
 				}
 			} else {
-				log.Fatalf("[%s] is an invalid file extension, expected [%s]", extension, fileExtension)
+				log.Fatalf("error while reading line from file [%s]", err.Error())
 			}
 		} else {
-
-			var errorText string
-			if err != nil {
-				errorText = err.Error()
-			}
-
-			log.Fatalf("error occurred while validating file: [%s]", errorText)
+			log.Fatalf("unable to open file [%s]: [%s]", path, err.Error())
 		}
 	} else {
 		log.Fatalf("--file path argument is required")
 	}
 }
 
-func buildLine(graph *graph.Graphy, line string) (err error) {
+func loadFile(path string) (file *os.File, reader *bufio.Reader, err error) {
+	var valid bool
+	if valid, err = validFile(path); valid && err == nil {
+
+		var extension = filepath.Ext(path)
+		if fileExtension == extension {
+
+			// Load the file
+			if file, err = os.Open(path); err == nil {
+
+				reader = bufio.NewReader(file)
+			} else {
+				err = errors.Errorf("error while reading from file [%s] : [%s]", path, err.Error())
+			}
+		} else {
+			err = errors.Errorf("[%s] is an invalid file extension, expected [%s]", extension, fileExtension)
+		}
+	} else {
+
+		var errorText string
+		if err != nil {
+			errorText = err.Error()
+		}
+
+		err = errors.Errorf("error occurred while validating file: [%s]", errorText)
+	}
+
+	return file, reader, err
+}
+
+func buildGraph(line string) (graphy *graph.Graphy, err error) {
+
+	line = clean(line)
+
+	// Parse file
+	if len(line) > 0 {
+		// Parse out the header of the file
+		values := strings.Split(line, " ")
+
+		// The header should only be two fields
+		if len(values) == 2 {
+			wedgy := false
+			dedgy := false
+
+			if values[0] == directed {
+				dedgy = true
+			}
+
+			if values[1] == weighted {
+				wedgy = true
+			}
+
+			graphy = &graph.Graphy{
+				Directional: dedgy,
+				Weighted:    wedgy,
+			}
+		} else {
+			err = errors.New("file is empty")
+		}
+	} else {
+		err = errors.New("file is empty")
+	}
+
+	return graphy, err
+}
+
+func buildNode(graphy *graph.Graphy, line string) (err error) {
 	clean(line)
 
 	if len(line) > 0 {
 		values := strings.Split(line, "=")
 		if len(values) <= 3 {
-			if graph.Weighted && len(values) == 3 {
+			if graphy.Weighted && len(values) == 3 {
 				// Weighted Graph
 
-			} else if !graph.Weighted && len(values) == 2 {
+			} else if !graphy.Weighted && len(values) == 2 {
 				// Unweighted Graph
 
 			} else if len(values) == 1 {
 				// Node w/out edges
-				graph.AddNode(values[0])
+				graphy.AddNode(values[0])
 			} else {
 				err = errors.Errorf("line [%s] is malformed for the graph type", line)
 			}
